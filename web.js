d2a652042d7a49891982acf1795074eaca83b3ab
Joe Chung <joechung91@gmail.com>
2013-07-13 03:46:37 +0000

    revised web.js

--- web.js
+++ web.js
@@ -2,10 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var data = fs.readFileSync('index.html', {encoding: 'utf8'});
 
 app.get('/', function(request, response) {
-  response.send(data);
+  var fs = require('fs');
+  var data = fs.readFileSync('index.html');
+  response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
